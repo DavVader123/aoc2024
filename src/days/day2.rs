0844342b569243159ef8966f8afd0ab679c71d9c
david <david.m120604@gmail.com>
2024-12-02 16:25:26 +0100

    day 2 part1

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -3,12 +3,50 @@ pub fn solve(input: String) -> (usize, usize) {
 	let part2 = solve_part2(input.clone()); 
     (part1, part2) 
 } 
- 
-fn solve_part1(input: String) -> usize {
-	for report in input.lines() {
-		report.split(' ').for_each();
+
+fn parse_input(input: String) -> Vec<Vec<usize>> {
+	let mut data: Vec<Vec<usize>> = Vec::new();
+	for report_line in input.lines() {
+		let mut report: Vec<usize> = Vec::new();
+		for num in report_line.split(' '){
+			report.push(num.parse().unwrap());
+		}
+		data.push(report);
 	}
-	0
+	data
+}
+
+fn sign(number: i32) -> i8 {
+	if number < 0 {
+		-1
+	} else if number > 0 {
+		1
+	} else { 
+		0
+	}
+}
+
+fn is_safe(report: Vec<usize>) -> bool {
+	let mut previous: i32 = 0;
+	for element in report.iter().enumerate() {
+		if (element.0 != 0) {
+			if ((*element.1) as i32).abs_diff(previous) < 1 || ((*element.1) as i32).abs_diff(previous) > 3 {
+				return false;
+			}
+			if sign((*element.1) as i32 - previous) != sign(report[1] as i32 - report[0] as i32) {
+				return false;
+			}
+		}
+		previous = *element.1 as i32;
+	}
+	true
+}
+
+fn solve_part1(input: String) -> usize {
+	let data: Vec<Vec<usize>> = parse_input(input);
+	let mut sum: usize = 0;
+	sum += data.iter().filter(|&report| {is_safe(report.clone())}).count();
+	sum
 } 
  
 fn solve_part2(input: String) -> usize { 
