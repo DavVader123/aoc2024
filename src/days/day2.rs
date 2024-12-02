6a0609ed3859b2ccbf3187fa8a5c36ac2c8a0396
david <david.m120604@gmail.com>
2024-12-02 07:44:29 +0100

    started day2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,11 +1,14 @@
-pub fn solve(input: String) -> (usize, usize) { 
+pub fn solve(input: String) -> (usize, usize) {
 	let part1 = solve_part1(input.clone()); 
 	let part2 = solve_part2(input.clone()); 
     (part1, part2) 
 } 
  
-fn solve_part1(input: String) -> usize { 
-0 
+fn solve_part1(input: String) -> usize {
+	for report in input.lines() {
+		report.split(' ').for_each();
+	}
+	0
 } 
  
 fn solve_part2(input: String) -> usize { 
