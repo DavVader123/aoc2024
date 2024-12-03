79f0282792f83cff540b228cd0805033129d9e41
david <david.m120604@gmail.com>
2024-12-03 07:16:39 +0100

    started day 3

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,13 +1,26 @@
-pub fn solve(input: String) -> (usize, usize) { 
+use aoc_parse::Parser;
+
+pub fn solve(input: String) -> (usize, usize) {
 	let part1 = solve_part1(input.clone()); 
 	let part2 = solve_part2(input.clone()); 
     (part1, part2) 
-} 
+}
+
+fn parse_input(input: String) -> Vec<String> {
+	let mut muls: Vec<String> = Vec::new();
+	let mut buf: String = String::new();
+	for c in input.chars() {
+		if c == 'm' || c == 'u' || c == 'l' || c == '(' || c == ',' || c == ')' || c.is_digit(10) {
+			buf.push(c.clone());
+		}
+	}
+	muls
+}
  
 fn solve_part1(input: String) -> usize { 
-0 
+	0
 } 
  
 fn solve_part2(input: String) -> usize { 
-0 
+	0
 } 
